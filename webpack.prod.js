4ca9559da108cba4f03dac6dc524b639ebf4f9c1
Kochkin Danila <>
2021-08-17 16:25:35 +0300

    Добавил terser-react-plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,16 @@
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
+const TerserPlugin = require("terser-webpack-plugin")
 
 module.exports = merge(common, {
     devtool: false,
     optimization: {
         minimize: true,
         minimizer: [
-            `...`,
+            new TerserPlugin({
+                extractComments: false
+            }),
             new CssMinimizerPlugin(),
         ],
         splitChunks: {
