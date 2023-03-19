5cffefbac3a6dc1fe80422015b7186de8cbf2888
Kochkin Danila <>
2023-03-19 20:04:09 +0300

    feat(Textbook): объекты основы

--- src/stories/Textbook/playground.ts
+++ src/stories/Textbook/playground.ts
@@ -1,2 +1,18 @@
 export const playground = () => {
+  const multiplyNumeric = (obj: Record<string, unknown>) => {
+    for (const objKey in obj) {
+      if (typeof obj[objKey] === 'number') {
+        (obj[objKey] as number) *= 2;
+      }
+    }
+    return obj;
+  };
+
+  let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+  };
+
+  console.info(multiplyNumeric(menu));
 };
