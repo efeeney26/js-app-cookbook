37ff32ca12c82b3e25198cd32f9a908b258686cb
Kochkin Danila <>
2023-03-26 15:27:24 +0300

    feat(Textbook): объекты методы и конструкторы

--- src/stories/Textbook/playground.ts
+++ src/stories/Textbook/playground.ts
@@ -1,18 +1,2 @@
 export const playground = () => {
-  const multiplyNumeric = (obj: Record<string, unknown>) => {
-    for (const objKey in obj) {
-      if (typeof obj[objKey] === 'number') {
-        (obj[objKey] as number) *= 2;
-      }
-    }
-    return obj;
-  };
-
-  let menu = {
-    width: 200,
-    height: 300,
-    title: "My menu"
-  };
-
-  console.info(multiplyNumeric(menu));
 };
